105893c3f22bfc2f2632450a22f4e6b4dff6f4ed
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-11-29 17:56:23 -0800

    Adds Header and Footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, {
+  Component
+} from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Author of this peculiar app: <a href="https://www.andrewenyeart.com">Andrew Enyeart</a>
+        </p>
+      </div>
+    )
+  }
+}
