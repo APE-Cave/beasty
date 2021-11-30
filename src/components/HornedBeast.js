c32c85ff1d80f9db3fc0be5ce65bf7088f59fa8a
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-11-30 01:59:03 -0800

    Stopping point for the night

---
+++ src/components/HornedBeast.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+
+export default class HornedBeast extends Component {
+  render() {
+    return (
+      <div>
+        <h2>{this.props.title}</h2>
+        <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title}/>
+        <p>{this.props.description}</p>
+      </div>
+    )
+  }
+}
