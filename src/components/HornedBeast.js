d077829b3a771649ff56ecce981416e7238275d4
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-11-30 22:19:48 -0800

    Gets click counter to update state

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,18 +1,23 @@
 import React, {Component} from "react";
 
 export default class HornedBeast extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-
-  //   }
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      clickCount:  0,
+    }
+  }
   
+  clickHandler = () => {
+    this.setState({clickCount: this.state.clickCount + 1,});
+    console.log(this.state.clickCount);
+  }
+
   render() {
     return (
       <div>
         <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title}/>
+        <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title} onClick={this.clickHandler}/>
         <p>{this.props.description}</p>
       </div>
     )
--- src/components/Main.js
+++ src/components/Main.js
@@ -15,16 +15,3 @@ export default class Main extends Component {
     );
   }
 }
-
-// export default class Main extends Component {
-//   render() {
-//     {
-//       data.map(beast => {
-//         return (
-//           <div>
-//             <HornedBeast title={beast.title} imageUrl={beast.image_url} description={beast.description} />
-//           </div>
-//         );
-//       })
-//     }
-//   }
