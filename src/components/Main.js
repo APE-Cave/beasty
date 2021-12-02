9b0858364847ab019eede36946d3958a474fa401
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-12-01 17:04:16 -0800

    Adds pseudocode outline of work

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,6 @@ export default class Main extends Component {
   render() {
     return (
       <div>
-      
         {
           this.props.data.map(beast => {
             return (<HornedBeast title={beast.title} imageUrl={beast.image_url} description={beast.description} />);
--- src/components/SelectedBeast.js
+++ src/components/SelectedBeast.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
-
+import heart from './img/heart.png';
 export default class SelectedBeast extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // display: false,
+    }
+  }
+
   render() {
     return (
       <div>
-        
+        <h2>{this.props.title}</h2>
+        <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title} onClick={this.clickHandler} />
+        <p>
+          <img src={heart} alt='heart icon' />
+          Likes: {this.state.clickCount}
+        </p>
+        <p>{this.props.description}</p>
       </div>
     )
   }
 }
+  // pass renderSelected from app to main, to HornedBeast
+  // when clickHandler runs in HornedBeast, invoke renderSelected with that HornedBeast's info (from this.props)
+  // renderSelected should switch on visibility of Modal and update beast data values via changing App.state
