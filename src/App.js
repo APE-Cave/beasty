e10204ce76f6017203656795a64755e8affc1d36
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-12-02 01:00:27 -0800

    Removes commented-out code

--- src/App.js
+++ src/App.js
@@ -32,12 +32,3 @@ export default class App extends Component {
     )
   }
 }
-
-// EXAMPLE BEAST FORMAT
-// {
-//   "image_url": "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
-//   "title": "UniWhal",
-//   "description": "A unicorn and a narwhal nuzzling their horns",
-//   "keyword": "narwhal",
-//   "horns": 1
-// },
