415e00ff7d58cb8255e144a26f59d5bdf010a177
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-12-02 16:20:54 -0800

    Implements bootstrap card and button for rendering HornedBeast

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import heart from './img/heart.png';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 export default class HornedBeast extends Component {
   constructor(props) {
@@ -18,15 +20,32 @@ export default class HornedBeast extends Component {
   // DONE pass entire beast object through main to this cmpnt
   render() {
     return (
-      <div>
-        <h2>{this.props.beast.title}</h2>
-        <img src={this.props.beast.image_url} alt={this.props.beast.description} title={this.props.beast.title} onClick={this.clickHandler} />
-        <p>
-          <img src={heart} alt='heart icon' />
-          Likes: {this.state.clickCount}
-        </p>
-        <p>{this.props.beast.description}</p>
-      </div>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={this.props.beast.image_url} alt={this.props.beast.description} title={this.props.beast.title} />
+        <Card.Body>
+          <Card.Title>{this.props.beast.title}</Card.Title>
+          <Card.Text>
+            {this.props.beast.description}
+          </Card.Text>
+          <div>
+            <img src={heart} style={{ maxWidth: '10%', margin: '.5rem'}} alt='heart icon'/>
+            Likes: {this.state.clickCount}
+          </div>
+          <Button onClick={this.clickHandler} variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+
+
+
+      // <div>
+      //   <h2>{this.props.beast.title}</h2>
+      //   <img src={this.props.beast.image_url} alt={this.props.beast.description} title={this.props.beast.title} onClick={this.clickHandler} />
+      //   <p>
+      //     <img src={heart} alt='heart icon' />
+      //     Likes: {this.state.clickCount}
+      //   </p>
+      //   <p>{this.props.beast.description}</p>
+      // </div>
     )
   }
 }
